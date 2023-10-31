cb3384681cc930555e5f7956e615d475c0697245
Toria Sellers <victoria.12292001@gmail.com>
2023-10-31 10:53:56 -0400

    exercises

---
+++ Exercises/ObjectScripts/ArrayScripts/avg_scores.js
@@ -0,0 +1,18 @@
+// Exercise 2
+
+let myScores = [97, 97, 98, 92, 100, 99, 96];
+let yourScores = [81, 82, 82, 84, 85, 72, 71];
+
+function getAverage(scores){
+    let sum = 0;
+    
+ for (let i = 0; i < scores.length; i++){
+    sum += scores[i];
+ 
+}
+    let avg = sum / scores.length;
+    return avg.toFixed(2);
+}
+
+console.log("My Average Score: " + getAverage(myScores));
+console.log("Your Average Score: " + getAverage(yourScores));
---
+++ Exercises/ObjectScripts/ArrayScripts/my_family.js
@@ -0,0 +1,12 @@
+
+let myFamily = [
+    "Jerry",
+    "Tina",
+    "Steph",
+    "Jason",
+    "Steven",
+]
+
+for (let i = 0; i < myFamily.length; i++) {
+console.log(myFamily);
+}
